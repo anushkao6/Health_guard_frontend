326b2498a28ab1ae3958e2b30b5999a8dc9fce44
Anushka Jadhav <anushkajadhav.17j@gmail.com>
2026-04-14 22:54:44 +0530

    Fix frontend API base

--- js/config.js
+++ js/config.js
@@ -1,10 +1,4 @@
 (function () {
-  // If the page is opened as a local file, default to the standard Spring Boot backend URL.
-  // Otherwise, use the same origin as the frontend.
-  const origin = window.location.origin;
-  const isLocalFile = origin.startsWith('file://');
-  
-  window.HG_API_BASE = window.HG_API_BASE || (isLocalFile ? 'http://localhost:8081/api' : `${origin}/api`);
-  
+  window.HG_API_BASE = 'https://healthguard-production-38dd.up.railway.app/api';
   console.log('HealthBot API Base:', window.HG_API_BASE);
 })();
